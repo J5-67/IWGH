22e257da84046bf7bce273bc567dd230573f38ee
새싹 <gjong567@gmail.com>
2026-02-11 03:16:53 +0900

    Update 과제는 끝나지 않아!(17952).cs

--- Silver/과제는 끝나지 않아!(17952).cs
+++ Silver/과제는 끝나지 않아!(17952).cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Baekjoon
+namespace ConsoleApp1
 {
     class Work
     {
@@ -17,43 +17,64 @@ namespace Baekjoon
         public int workScore = 0;
         public int workMinute = 0;
 
-
+        public Work(int score, int minute)
+        {
+            state = isWork.work;
+            workScore = score;
+            workMinute = minute;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            /*
-             * 과제는 가장 최근에 나온 순서대로 한다. 또한 과제를 받으면 바로 시작한다.
-             * 과제를 하던 도중 새로운 과제가 나온다면, 하던 과제를 중단하고 새로운 과제를 진행한다.
-             * 새로운 과제가 끝났다면, 이전에 하던 과제를 이전에 하던 부분부터 이어서 한다.
-             */
+            Stack<Work> workStack = new Stack<Work>();
 
-            Work w = new Work();
-            List<Work> workList = new List<Work>();
-
+            int totalScore = 0;
             int minute = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < minute; i++)
             {
                 string[] work = Console.ReadLine().Split(' ');
 
-                if(int.Parse(work[0]) == 0)
+                if (int.Parse(work[0]) == 1)
                 {
-                    w.state = Work.isWork.notwork;
-                    w.workScore = 0;
-                    w.workMinute = 0;
+                    Work newWork = new Work(int.Parse(work[1]), int.Parse(work[2]));
+
+                    workStack.Push(newWork);
+
+                    if(workStack.Count > 0)
+                    {
+                        Work currentWork = workStack.Peek();
+
+                        currentWork.workMinute--;
+
+                        if(currentWork.workMinute == 0)
+                        {
+                            totalScore += currentWork.workScore;
+                            workStack.Pop();
+                        }
+                    }
                 }
                 else
                 {
-                    w.state = Work.isWork.work;
-                    w.workScore = int.Parse(work[1]);
-                    w.workMinute = int.Parse(work[2]);
+                    if (workStack.Count > 0)
+                    {
+                        Work currentWork = workStack.Peek();
+
+                        currentWork.workMinute--;
+
+                        if (currentWork.workMinute == 0)
+                        {
+                            totalScore += currentWork.workScore;
+                            workStack.Pop();
+                        }
+                    }
                 }
-                
-                Console.WriteLine($"{w.state} {w.workScore} {w.workMinute}");
             }
+
+            Console.WriteLine(totalScore);
         }
     }
 }
