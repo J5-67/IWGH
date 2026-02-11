c9ee7c80af80ce34fc1ef7f4053682f31b906803
새싹 <gjong567@gmail.com>
2026-02-11 13:11:29 +0900

    Create 마이크로소프트 로고(5338).cs

---
+++ Bronze/마이크로소프트 로고(5338).cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       _.-;;-._");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-;;-._|");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-''-._|");
+        }
+    }
+}
