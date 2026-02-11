f0ecfef4e856baa13ce9d55452e5a05f41a5d7d6
새싹 <gjong567@gmail.com>
2026-02-11 13:24:02 +0900

    Create UCPC에서 가장 쉬운 문제 번호는?(25311).cs

---
+++ Bronze/UCPC에서 가장 쉬운 문제 번호는?(25311).cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch(int.Parse(Console.ReadLine()))
+            {
+                case 2018:
+                    Console.WriteLine("A");
+                    break;
+                case 2019:
+                    Console.WriteLine("A");
+                    break;
+                case 2020:
+                    Console.WriteLine("A");
+                    break;
+                case 2021:
+                    Console.WriteLine("A");
+                    break;
+                case 2022:
+                    Console.WriteLine("A");
+                    break;
+            }
+        }
+    }
+}
